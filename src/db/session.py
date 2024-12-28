07a56269835e857f70fc5ffe7dd6e19e00d4b2ce
Jason <jasonwadejob@gmail.com>
2024-12-28 11:06:58 -0800

    add schemas and created get_db

--- src/db/session.py
+++ src/db/session.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from typing import Generator
 
 from core.config import settings
 
@@ -8,3 +9,11 @@ print("Database URL is ", SQLALCHEMY_DATABASE_URL)
 engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def get_db() -> Generator:
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
