471e89475cd99bccb790870385858aedba4e7c53
Jason <jasonwadejob@gmail.com>
2024-12-28 11:45:00 -0800

    add create_user route

--- src/schemas/user.py
+++ src/schemas/user.py
@@ -5,3 +5,10 @@ class UserCreate(BaseModel):
     email: EmailStr
     password: str = Field(..., min_length=4)
 
+class ShowUser(BaseModel):
+    id: int
+    email: EmailStr
+    is_active: bool
+    
+    class Congif():
+        orm_mdoe = True
