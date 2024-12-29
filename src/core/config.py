cfeed71b458ecb8a783494f27819b7b15356eb74
Jason <jasonwadejob@gmail.com>
2024-12-28 20:08:13 -0800

    add security modele to enable create access token jwt

--- src/core/config.py
+++ src/core/config.py
@@ -3,16 +3,22 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 class Settings:
-    PROJECT_NAME:str = "FinancePro"
-    PROJECT_VERSION:str = "1.0.0"
-    
+    PROJECT_NAME: str = "FinancePro"
+    PROJECT_VERSION: str = "1.0.0"
+
     POSTGRES_USER: str = os.getenv("POSTGRES_USER")
     POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
     POSTGRES_SERVER: str = os.getenv("POSTGRES_SERVER", "localhost")
     POSTGRES_PORT: str = os.getenv("POSTGRES_PORT", 5432)
-    POSTGRES_DB: str = os.getenv('POSTGRES_DB', 'tdd')
+    POSTGRES_DB: str = os.getenv("POSTGRES_DB", "tdd")
     DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
     print(DATABASE_URL)
-    
+
+    SECRET_KEY: str = os.getenv("SECRET_KEY")
+    ALGORITHM = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
 settings = Settings()
